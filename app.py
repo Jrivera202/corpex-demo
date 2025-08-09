da67ebc970d3de1ab2cb8b3944ab2f55c1ce85c6
Jrivera202 <jhrc44401@hotmail.com>
2025-08-09 11:12:09 -0600

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request, redirect, url_for, session, f
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
-app = Flask(_name_)
+app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY", "devsecret")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "sqlite:///corpex_demo.db")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
