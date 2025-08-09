971e5c43ef5664e23a9101a850295c6619c35a0c
Jrivera202 <jhrc44401@hotmail.com>
2025-08-09 12:12:26 -0600

    Update app.py

--- app.py
+++ app.py
@@ -104,5 +104,5 @@ def reportes():
     balance = ventas_total - gastos_total
     return render_template("reportes.html", ventas_total=ventas_total, gastos_total=gastos_total, balance=balance)
 
-if _name_ == "_ _main_ _":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
